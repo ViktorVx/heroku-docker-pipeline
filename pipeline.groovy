ff60b8aefb767df76b841f6674ad19f6a1718663
ViktorVx <victorptrv@yandex.ru>
2021-07-29 20:24:41 +0300

    Simple pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,3 +1,10 @@
 pipeline {
-
+    agent java-11-docker-worker
+    stages {
+        stage("Hello world stage") {
+            steps {
+                echo "Hello world!"
+            }
+        }
+    }
 }
