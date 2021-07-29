c41ab0fd43d2c42a2203a9b9a427615516023d7b
ViktorVx <victorptrv@yandex.ru>
2021-07-29 20:41:23 +0300

    Add repo clone stage

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,9 +3,14 @@ pipeline {
         label 'java-11-docker-worker'
     }
     stages {
-        stage("Hello world stage") {
+        stage("Clone repository") {
             steps {
-                echo "Hello world!"
+                git url: 'https://github.com/ViktorVx/wordsFromWordBruteForce.git'
+            }
+        }
+        stage("Package jar") {
+            steps {
+                sh "ls -la"
             }
         }
     }
