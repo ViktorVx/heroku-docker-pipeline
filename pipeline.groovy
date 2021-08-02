c45cfbcc074896c9ec038ba2809e101a2d92ff20
ViktorVx <victorptrv@yandex.ru>
2021-08-02 22:09:04 +0300

    Add building of docker image

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,12 +3,12 @@ pipeline {
         label 'java-11-docker-worker'
     }
     stages {
-        stage("Clone repository") {
+//        stage("Clone repository") {
 //            steps {
 //                checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-user-github', url: 'https://github.com/ViktorVx/wordsFromWordBruteForce.git']]])
 //
 //            }
-        }
+//        }
         stage("Docker build") {
             steps {
                 script {
