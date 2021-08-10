e7d6ae27b08666d348f0a987f1d125d1f6aca710
ViktorVx <victorptrv@yandex.ru>
2021-08-10 22:15:58 +0300

    Add docker login to Heroku

--- pipeline.groovy
+++ pipeline.groovy
@@ -29,6 +29,8 @@ pipeline {
             steps {
                 script {
                     sh(script:'docker build -t registry.heroku.com/wfwbf-docker/web .')
+                    sh(script:'echo ${HEROKU_USER_NAME}')
+                    sh(script:'echo ${HEROKU_TOKEN}')
                     sh(script:'docker login --username=${HEROKU_USER_NAME} --password=${HEROKU_TOKEN} registry.heroku.com')
                     sh(script:'docker push registry.heroku.com/wfwbf-docker/web')
                 }
