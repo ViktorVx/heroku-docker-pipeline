f74eb2bd8ebec355f72053963038bd614fc9f51d
ViktorVx <victorptrv@yandex.ru>
2021-08-11 19:49:05 +0300

    Add Heroku api key

--- pipeline.groovy
+++ pipeline.groovy
@@ -36,9 +36,9 @@ pipeline {
 
                     withCredentials([string(credentialsId: 'HEROKU_API_TOKEN', variable: 'SECRET2')]) {
                         sh(script:'export HEROKU_API_KEY=${SECRET2}')
+                        sh(script:'heroku container:login')
+                        sh(script:'heroku container:release web --app ${HEROKU_APP}')
                     }
-                    sh(script:'heroku container:login')
-                    sh(script:'heroku container:release web --app ${HEROKU_APP}')
                 }
             }
         }
